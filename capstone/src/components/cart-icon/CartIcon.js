9114ae5046d46ca14b0d2a1060952d66cfca4a83
champak kumar <100565945+singlas106@users.noreply.github.com>
2022-09-13 11:00:36 +0530

    close cart dropdown by clicking anywhere functionality in cartIcon

--- capstone/src/components/cart-icon/CartIcon.js
+++ capstone/src/components/cart-icon/CartIcon.js
@@ -2,20 +2,32 @@ import React, { useContext } from 'react'
 import {CartIconContainer, ShoppingIcon, ItemCount} from './CartIcon.styles';
 // import { ReactComponent as ShoppingBag } from '../../assets/shopping.svg';
 import { CartContext } from '../../context/CartContext';
+import OutsideClickHandler from 'react-outside-click-handler';
 
 const CartIcon = () => {
     const {isCartOpen, setIsCartOpen, cartCount} = useContext(CartContext);
     const toggleIsCartOpen = () => {
         setIsCartOpen(!isCartOpen);
     }
-
-
+      
   return (
-    <CartIconContainer onClick={toggleIsCartOpen}>
-      <ShoppingIcon className='shopping-icon'/>
-      <ItemCount>{cartCount}</ItemCount>
-    </CartIconContainer>
+    <OutsideClickHandler
+     onOutsideClick = {() => {
+      if(isCartOpen){
+        setIsCartOpen(!isCartOpen)
+      }
+     }}
+    >
+      <CartIconContainer onClick={toggleIsCartOpen}>
+        <ShoppingIcon className='shopping-icon'/>
+        <ItemCount>{cartCount}</ItemCount>
+      </CartIconContainer>
+    </OutsideClickHandler>
   )
 }
 
 export default CartIcon
+
+
+
+  
