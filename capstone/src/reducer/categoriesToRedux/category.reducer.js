8096ce14235678cd0c41aea71ceeacbd05f61ca6
champak kumar <100565945+singlas106@users.noreply.github.com>
2022-09-17 20:36:17 +0530

    shop page also now working

--- capstone/src/reducer/categoriesToRedux/category.reducer.js
+++ capstone/src/reducer/categoriesToRedux/category.reducer.js
@@ -1,18 +1,20 @@
 import { CATEGORIES_TYPES } from "./category.types";
 
-
-
 export const CATEGORIES_INITIAL_STATE = {
-    categoriesMap: {},
-}
+  categoriesMap: {},
+};
 
-export const categoriesReducer = (state=CATEGORIES_INITIAL_STATE, action={}) => {
-    const {type, payload} = action;
-
-    switch(type){
-        case 'CATEGORIES_TYPES.SET_CATEGORIES_MAP':
-            return{...state, categoriesMap: payload}
-        default:
-            return state;       
-    }
+export const categoriesReducer = (
+  state = CATEGORIES_INITIAL_STATE,
+  action = {}
+) => {
+  const { type, payload } = action;
+  console.log(state);
+  console.log(action);
+  switch (type) {
+    case CATEGORIES_TYPES.SET_CATEGORIES_MAP:
+      return { ...state, categoriesMap: payload };
+    default:
+      return state;
+  }
 };
