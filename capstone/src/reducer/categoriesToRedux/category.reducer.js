ede6fd46a74bb2282f39cd943eabc19f7153c131
champak kumar <100565945+singlas106@users.noreply.github.com>
2022-09-20 12:44:01 +0530

    .

--- capstone/src/reducer/categoriesToRedux/category.reducer.js
+++ capstone/src/reducer/categoriesToRedux/category.reducer.js
@@ -10,7 +10,7 @@ export const categoriesReducer = (state=CATEGORIES_INITIAL_STATE, action={}) => 
     const {type, payload} = action;
 
     switch(type){
-        case 'CATEGORIES_TYPES.SET_CATEGORIES_MAP':
+        case CATEGORIES_TYPES.SET_CATEGORIES_MAP:
             return{...state, categoriesMap: payload}
         default:
             return state;       
